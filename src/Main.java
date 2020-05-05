0dd9760266e0e578e5b8d044d46d32895c2d2bf2
abedaton <antoine.bedaton@hotmail.fr>
2020-05-05 19:24:00 +0200

    removed files

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,11 @@ public class Main {
         ArrayList<Person> people = parser.parseName_basic("datasets/name.basics.tsv", 100000);
         HashMap<String, String> translatedFilm = parser.parseTitleAkas("datasets/title.akas.tsv", "US");
 
-        BeaconGraph beaconGraph = new BeaconGraph(people, translatedFilm);
-//        String path = beaconGraph.pathToBacon("Morgan Freeman");
+        BaconGraph baconGraph = new BaconGraph(people, translatedFilm);
+//        String path = baconGraph.pathToBacon("Morgan Freeman");
 //        System.out.println(path);
-        System.out.println(beaconGraph.pathFromActor("Natalie Portman", "Natalie Portman"));
-//        System.out.println(beaconGraph.diameter());
+        System.out.println(baconGraph.pathFromActor("Natalie Portman", "Natalie Portman"));
+//        System.out.println(baconGraph.diameter());
 
 
     }
